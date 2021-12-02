511a9b580b9b58becaad3c2b57b6a7c24ca4d72e
Moamen Mostafa <mmoamen270@gmail.com>
2021-12-02 18:54:15 +0200

    Algorithm Project1

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace algorithm
+{
+    class Program
+    {
+        static long LoopFactorial(int number)
+        {
+            long result = 1;
+            for (int i = number; i > 1; i--)
+            {
+                result *= i;
+            }
+            return result;
+        }
+
+        static long RecursionFactorial(int number)
+        {
+            if (number == 1) return 1;
+            return number * RecursionFactorial(number - 1);
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number from 1 to 20: ");
+            int number = int.Parse(Console.ReadLine());
+            Stopwatch loop = new Stopwatch();
+            loop.Start();
+            long result = LoopFactorial(number);
+            loop.Stop();
+
+            Console.WriteLine("\tTime with Loop Factorial = " + loop.Elapsed + "\n result = " + result + "\n");
+
+            Stopwatch Recursion = new Stopwatch();
+            Recursion.Start();
+            result = RecursionFactorial(number);
+            Recursion.Stop();
+            Console.WriteLine("\tTime with Recursion Factorial = " + Recursion.Elapsed + "\n result = " + result + "\n");
+
+
+        }
+    }
+}
